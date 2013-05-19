bd4210b316393e7ce64724e77a3fb353f8f0cce9
LastElb <igor.lueckel@googmail.com>
2013-05-19 13:46:59 +0200

    Initial code commit You need ActionBarSherlock as dependecy https://github.com/JakeWharton/ActionBarSherlock/tree/master/actionbarsherlock-samples

---
+++ src/com/swe/zim/MainActivity.java
@@ -0,0 +1,14 @@
+package com.swe.zim;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.Theme_Sherlock_Light);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
