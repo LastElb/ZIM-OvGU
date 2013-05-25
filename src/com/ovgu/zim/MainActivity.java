e64488e42691eeb5ec662a75c6724f347bea23eb
LastElb <igor.lueckel@gmail.com>
2013-05-26 01:32:29 +0200

    App is working again

--- src/com/ovgu/zim/MainActivity.java
+++ src/com/ovgu/zim/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ovgu.zim;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
@@ -11,9 +12,20 @@ public class MainActivity extends SherlockActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Check the user preferences (alarmtime,...)
+		SharedPreferences preferences = getSharedPreferences("appPreferences", 0);
+		// comment the next line if you want to keep the user preferences
+		//preferences.edit().clear().commit();
+		
+		//We are checking, if the usercode is set. If not, we show the PreferenceActivity.
+		if (preferences.getString("preferenceUserID", "") == ""){
+			Intent intent = new Intent(this, PreferenceActivity.class);
+			startActivity(intent);
+		}
 	}
 
-	 @Override
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		 //Used to put dark icons on light action bar
 		 return true;
--- src/com/ovgu/zim/PreferenceActivity.java
+++ src/com/ovgu/zim/PreferenceActivity.java
@@ -11,18 +11,88 @@ import com.actionbarsherlock.view.MenuItem;
 
 public class PreferenceActivity extends SherlockPreferenceActivity  {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_preference);
+		addPreferencesFromResource(R.xml.preferences);
+		final String changedWakeTime = this.getString(R.string.PreferenceWakeTimeSet);
+		final String changedUserID = this.getString(R.string.PreferenceUserIDSet);
+		//This is called when the UserID changed
+		this.findPreference("preferenceUserID").setOnPreferenceChangeListener(new OnPreferenceChangeListener(){
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                preference.setSummary(changedUserID + " " + newValue.toString());
+                SetDoneIcon(preference);
+                return true;
+            }
+        });
+		
+		//The following four constructs get called when the selected alarm time changes
+		this.findPreference("WakeTime1").setOnPreferenceChangeListener(
+				new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						preference.setSummary(changedWakeTime + " "
+								+ newValue.toString() + ":00 Uhr");
+						SetDoneIcon(preference);
+						return true;
+					}
+				});
+		
+		this.findPreference("WakeTime2").setOnPreferenceChangeListener(
+				new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						preference.setSummary(changedWakeTime + " "
+								+ newValue.toString() + ":00 Uhr");
+						SetDoneIcon(preference);
+						return true;
+					}
+				});
+		
+		this.findPreference("WakeTime3").setOnPreferenceChangeListener(
+				new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						preference.setSummary(changedWakeTime + " "
+								+ newValue.toString() + ":00 Uhr");
+						SetDoneIcon(preference);
+						return true;
+					}
+				});
+
+		this.findPreference("WakeTime4").setOnPreferenceChangeListener(
+				new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						preference.setSummary(changedWakeTime + " "
+								+ newValue.toString() + ":00 Uhr");
+						SetDoneIcon(preference);
+						return true;
+					}
+				});
+	}
+	
+	@SuppressLint("NewApi")
+	private void SetDoneIcon(Preference preference){
+		if (android.os.Build.VERSION.SDK_INT>android.os.Build.VERSION_CODES.HONEYCOMB){
+        	preference.setIcon(R.drawable.ic_navigation_accept);
+        }
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		menu.add("Save")
-        //.setIcon(R.drawable.ic_compose_inverse)
-        .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+				.setIcon(R.drawable.ic_content_save)
+				.setShowAsAction(
+						MenuItem.SHOW_AS_ACTION_IF_ROOM
+								| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		return super.onCreateOptionsMenu(menu);
 	}
 
